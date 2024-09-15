e9998d7082ff4c8f1e9618ec22a3f0b8c95de5eb
Luis Carlos <lcsb.0190@gmail.com>
2024-09-14 21:23:00 -0500

    Abordamos la sesión 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI # Import la clase fastapi de la librería fastapi
+from fastapi.responses import HTMLResponse # Import la clase HTMLResponse de la librería fastapi.responses
 
+from movies_list import movies_list # Importar la variable movies_list
 app = FastAPI() # Crear una instancia de la clase fastapi
 
 app.title = 'Mi Primera Aplicación de Películas y Análisis de Datos' # Asignar un valor a la variable title
@@ -8,6 +10,14 @@ app.version = '0.0.1' # Asignar un valor a la variable version
 @app.get('/', tags=['Home']) # estamos definiendo una ruta
 
 def message(): # definimos una función de la ruta
-    return 'Hello World' # retornamos un diccionario
+    return HTMLResponse('<h1>Hello, World!</h1>') # retorna un objeto de la clase HTMLResponse
 
-
+@app.get('/movies', tags=['Movies']) # estamos definiendo una ruta de la clase fastapi
+def movies(): # definimos una función de la ruta
+    return movies_list
+@app.get('/movies/{id}', tags=['Movies']) # estamos definiendo una ruta para consultar peliculas por id
+def get_movie(id:int): # definimos una función de la rut
+    for item in movies_list:
+        if item['id'] == id:
+            return item
+    return []
---
+++ movies_list.py
@@ -0,0 +1,65 @@
+movies_list = [
+    {
+        "id": 1,
+        "title": "The Matrix",
+        "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the forces of...",
+        "year": 1999,
+        "rating": 8.7
+    },
+    {
+        "id": 2,        
+        "title": "The Godfather",
+        "overview": "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family...", 
+        "year": 1972,
+        "rating": 9.2
+    },
+    {
+        "id": 3,
+        "title": "The Godfather: Part II",
+        "overview": "In the continuing saga of the Corleone crime family, a young Vito Corleone grows up in Sicily and in 1910s New York. In...",
+        "year": 1974,
+        "rating": 9.0
+    },
+    {
+        "id": 4,
+        "title": "The Dark Knight",
+        "overview": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come...",
+        "year": 2008,
+        "rating": 9.0
+    },
+    {
+        "id": 5,
+        "title": "12 Angry Men",
+        "overview": "The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces...",
+        "year": 1957,
+        "rating": 8.9
+    },
+    {
+        "id": 6,
+        "title": "Schindler's List",
+        "overview": "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for...",
+        "year": 1993,
+        "rating": 8.9
+    },
+    {
+        "id": 7,
+        "title": "The Lord of the Rings: The Return of the King",
+        "overview": "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they...",
+        "year": 2003,
+        "rating": 8.9
+    },
+    {
+        "id": 8,
+        "title": "Pulp Fiction",
+        "overview": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in...",
+        "year": 1994,
+        "rating": 8.9
+    },
+    {
+        "id": 9,
+        "title": "The Good, the Bad and the Ugly",
+        "overview": "A bounty hunting scam joins two men in an uneasy alliance against a third in a state of chaos,...",
+        "year": 1966,
+        "rating": 8.8
+    }        
+]
